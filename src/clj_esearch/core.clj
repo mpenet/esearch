19914b068e368f841b5b6fc706fb7ae9c3c84cd9
Max Penet <max.penet@gmail.com>
2011-06-12 21:07:09 +0200

    project name change + cleanup of direct mode testing ground

---
+++ src/clj_esearch/core.clj
@@ -0,0 +1,100 @@
+(ns clj-esearch.core
+  (:use [aleph.http :only [sync-http-request http-request]]
+        [lamina.core :only [closed-channel]]
+        [clojure.contrib.json :only [json-str]]))
+
+(defn str-join [value separator]
+  (apply str (interpose separator value)))
+
+(defn url
+  "Build urls by joining params with /
+   Treats vector args as multi params separated by ,
+   and :all as _all"
+  [& parts]
+  (str-join (->> parts
+                 (remove nil?)
+                 (map #(cond (sequential? %)
+                             (str-join % ",")
+                             (= :all %)
+                             "_all"
+                             :else %)))
+            "/"))
+
+(defn query
+  [params]
+  (assoc params
+    :headers {"content-type" "application/json"}
+;    :keep-alive? true
+    :auto-transform true))
+
+(defn request
+  "Forwards the ring requests settings to the appropriate request client"
+  [params async]
+  (apply (if async http-request sync-http-request)
+         [(query params)]))
+
+(defn add-doc
+  [server index type doc & {:keys [id query-string async]}]
+  (request {:method :post
+            :url (apply url (if id
+                              [server index type id]
+                              [server index type]))
+            :query-string query-string
+            :body (closed-channel doc)}
+           async))
+
+(defn get-doc
+  [server index type id & {:keys [query-string async]}]
+  (request {:method :get
+            :url (url server index type id)
+            :query-string query-string}
+           async))
+
+(defn delete-doc
+  [server index type id & {:keys [query-string async]}]
+  (request {:method :delete
+            :url (url server index type id)
+            :query-string query-string}
+           async))
+
+(defn delete-by-query
+  [server delete-query & {:keys [index type query-string async]}]
+  (request {:method :delete
+            :url (url server index type "_query")
+            :query-string query-string
+            :body (closed-channel delete-query)}
+           async))
+
+(defn search-doc
+  [server search-query & {:keys [index type query-string async]}]
+  (request {:method :get
+            :url (url server index type  "_search")
+            :query-string query-string
+            :body (closed-channel search-query)}
+           async))
+
+(defn percolate
+  [server index name percolator-query & {:keys [query-string async]}]
+  (request {:method :put
+            :url (url server "_percolator" index name)
+            :query-string query-string
+            :body (closed-channel percolator-query)}
+           async))
+
+(defn count-docs
+  [server count-query & {:keys [index type query-string async]}]
+  (request {:method :get
+            :url (url server index type  "_count")
+            :query-string query-string
+            :body (closed-channel count-query)}
+           async))
+
+(defn bulk
+  [server item-coll & {:keys [query-string async]}]
+  (request {:method :put
+            :url (url server "_bulk")
+            :query-string query-string
+            :body (str-join (map #(json-str %) item-coll) "\n")}
+           async))
+
+
--- test/clj_esearch/test/core.clj
+++ test/clj_esearch/test/core.clj
@@ -1,18 +1,18 @@
-(ns elaticlj.test.core
-  (:use [elaticlj.core]
-        [record-utils.core :only [def-record]]
+(ns clj-esearch.test.core
+  (:use [clj-esearch.core]
         [lamina.core :only [enqueue enqueue-and-close named-channel
                             receive run-pipeline wait-for-result
                             read-channel]]
         [aleph.formats :only [byte-buffer->string]]
         [clojure.test]))
 
-(def test-server "http://localhost:9200")
+(def test-server "http://127.0.0.1:9200")
 (def test-index "test-index")
 (def test-type "test-type")
 
-(def-record Doc [title posted content])
-(def test-doc (make-doc {:title "foo" :posted 12345 :content "bar"}))
+(defrecord Doc [title posted content])
+(def test-doc (Doc. "foo" 12345 "bar"))
+(def test-doc2 (Doc. "foo bar"  123456  "bar baz"))
 
 (use-fixtures :each (fn [atest]
                      (atest)
@@ -45,11 +45,18 @@
                       test-type
                       (assoc test-doc :content "prout")
                       :id 2)
-        response (search-doc test-server test-index test-type
+        doc2 (add-doc test-server
+                      "test-index2"
+                      test-type
+                      (assoc test-doc :content "prout")
+                      :id 2)
+        response (search-doc test-server
                              {:query {:term {:content "prout"}}}
-                              :query-string {:pretty true})]
-      (is (= 200 (:status response)))
-      (is (= 1 (-> response :body :hits :hits count)))))
+                             :index [test-index "test-index2"]
+                             :type test-type
+                             :query-string {:pretty true})]
+    (is (= 200 (:status response)))
+    (is (= 2 (-> response :body :hits :hits count)))))
 
 
 (deftest search-async-test
@@ -59,22 +66,12 @@
                      (assoc test-doc :content "prout")
                       :id 2)
         response (wait-for-result (search-doc test-server
-                                              test-index
-                                              test-type
                                               {:query {:term {:content "prout"}}}
-                                               :async true))]
+                                              :index test-index
+                                              :type test-type
+                                              :async true))]
     (is (= 200 (:status response)))
     (is (= 1 (-> response :body :hits :hits count)))))
 
-
-(deftest performance-test
-  (time
-   (dotimes [i 100]
-     (add-doc test-server
-              test-index
-              test-type
-              test-doc
-              :async true)))
-
-   (is true))
-
+(deftest percolate-test
+  (is (= 200 (:status (percolate test-server test-index "perc-test" {:query {:term {:field1 "value1" }}})))))

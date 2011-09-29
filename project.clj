7ee75d628cde0f77f1783f5488621d2d1e41cbde
Max Penet <max.penet@gmail.com>
2011-09-29 09:24:45 +0200

    freeze version to non snapshot

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject clj-esearch "1.0.0-SNAPSHOT"
+(defproject clj-esearch "0.0.1"
 
   :description "FIXME: write description"
 
   :repositories { "sonatype.org" "http://oss.sonatype.org/content/repositories/releases/" }
 
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [aleph "0.2.0-beta1"]]
+                 [aleph "0.2.0-beta2"]]
 
   :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT" :exclusions
                       [org.clojure/clojure org.clojure/clojure-contrib]]

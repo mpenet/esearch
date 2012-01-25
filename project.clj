b7542c7b6b4b21e42963d92a4f2a1111ca1955e5
Max Penet <max.penet@gmail.com>
2012-01-25 09:19:03 +0100

    update aleph to 0.2.1-alpha1

--- project.clj
+++ project.clj
@@ -1,11 +1,7 @@
-(defproject clj-esearch "0.3.0"
+(defproject clj-esearch "0.3.1"
 
   :description "Elastic Search REST client for Clojure"
 
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [cheshire "2.0.4"]
-                 [aleph "0.2.0"]]
-
-  :dev-dependencies [[swank-clojure "1.3.4" :exclusions
-                      [org.clojure/clojure org.clojure/clojure-contrib]]
-                     [clojure-source "1.2.0"]])
+                 [aleph "0.2.1-alpha1"]])

6d03aced8b1961664c1d17cbc0490ff9e0daf224
Max Penet <max.penet@gmail.com>
2012-01-25 09:20:13 +0100

    update cheshire to 2.0.6

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :description "Elastic Search REST client for Clojure"
 
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [cheshire "2.0.4"]
+                 [cheshire "2.0.6"]
                  [aleph "0.2.1-alpha1"]])

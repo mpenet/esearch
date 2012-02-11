654c5b96e75f4ef79779ae3580d33be2528afaf8
Max Penet <max.penet@gmail.com>
2012-02-11 20:46:43 +0100

    clj-1.3

--- project.clj
+++ project.clj
@@ -1,7 +1,5 @@
-(defproject clj-esearch "0.3.2"
-
+(defproject clj-esearch "0.3.3"
   :description "Elastic Search REST client for Clojure"
-
-  :dependencies [[org.clojure/clojure "1.2.1"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [cheshire "2.0.6"]
-                 [aleph "0.2.1-alpha1"]])
+                 [aleph "0.2.1-alpha2-SNAPSHOT"]])

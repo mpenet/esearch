3e7d9825e48a355155ed362d74309b037eb2f8ce
Max Penet <m@qbits.cc>
2012-11-08 19:04:48 +0100

    bump aleph dependency

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject cc.qbits/clj-esearch "0.5.3"
+(defproject cc.qbits/clj-esearch "0.5.4"
   :description "Elastic Search REST client for Clojure"
   :url "https://github.com/mpenet/clj-esearch"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [cheshire "4.0.2"]
-                 [aleph "0.3.0-beta4"]])
+                 [aleph "0.3.0-beta7"]])

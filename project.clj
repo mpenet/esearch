463d4b1796d764637eee945065ad614ce9c8a1c6
Max Penet <m@qbits.cc>
2014-12-25 20:38:31 +0100

    update to latest jet (fixed chunked response issues)

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject cc.qbits/esearch "0.8.2"
+(defproject cc.qbits/esearch "0.8.3"
   :description "Elastic Search REST client for Clojure"
   :url "https://github.com/mpenet/clj-esearch"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [cc.qbits/jet "0.5.0-alpha2"]])
+                 [cc.qbits/jet "0.5.3"]])

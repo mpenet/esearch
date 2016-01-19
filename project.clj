736cd03aec324309b4bc0a509170f02819b73027
Max Penet <m@qbits.cc>
2016-01-19 13:41:57 +0100

    bump

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject cc.qbits/esearch "0.9.2"
+(defproject cc.qbits/esearch "0.9.3"
   :description "Elastic Search REST client for Clojure"
   :url "https://github.com/mpenet/clj-esearch"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-beta2"]
-                 [cc.qbits/jet "0.7.2"]])
+                 [cc.qbits/jet "0.7.3"]])
